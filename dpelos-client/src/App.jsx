d39324a25b231a123ca0b1046d83cb5b1c26cedd
Mauricio.M <m.morocho1196@gmail.com>
2025-04-17 18:34:19 -0500

    initialized react project with vite and tailwind

---
+++ dpelos-client/src/App.jsx
@@ -0,0 +1,11 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css';
+
+function App() {
+  return (
+    <h1 className='underline'>Hello world</h1>
+  );
+}
+
+export default App;
