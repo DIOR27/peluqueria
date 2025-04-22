e13dc1a37ace3962cc1e8da7166f035087f9890b
Mauricio.M <m.morocho1196@gmail.com>
2025-04-21 21:25:41 -0500

    Fixed link

--- dpelos-client/src/components/dashboard/Sidebar.jsx
+++ dpelos-client/src/components/dashboard/Sidebar.jsx
@@ -78,7 +78,7 @@ export default function Sidebar() {
                 </button>
               </MenuItem>
               <MenuItem className="w-[180px] px-4 py-2 cursor-pointer">
-                <Link className=" data-[focus]:bg-gray-100 flex items-center gap-2" href="/dashboard/settings">
+                <Link className=" data-[focus]:bg-gray-100 flex items-center gap-2" to="/dashboard/settings">
                   <Settings className="h-4 w-4" />
                   Configuración
                 </Link>
