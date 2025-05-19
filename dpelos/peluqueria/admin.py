31472827833e587147df6ff354de04eaf3840656
sinonxd <juaneskfire@gmail.com>
2025-05-19 09:20:36 -0500

    implementación horas disponibles en formulario de reservas para cliente y codigo de gestión de superuser para el django admin page

--- dpelos/peluqueria/admin.py
+++ dpelos/peluqueria/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Usuario
 
-# Register your models here.
+# lo dejo para gestionar los datos y permisos del superuser
+admin.site.register(Usuario, UserAdmin)
